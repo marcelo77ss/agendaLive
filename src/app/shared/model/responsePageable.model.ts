9b9154ec4bab4e9508b43caff440c6caf6a10274
marcelo77ss <marcelo.77ss@gmail.com>
2021-09-08 23:28:01 -0300

    primeiro commit

---
+++ src/app/shared/model/responsePageable.model.ts
@@ -0,0 +1,12 @@
+export class ResponsePageable{
+    content!: any[];
+    frist!: boolean;
+    last!: boolean;
+    number!: number;
+    numberOfelements!: number
+    pageable!: any[];
+    size!: number;
+    sort!: number;
+    totalElements!:number;
+    totalPages!: number;
+}
---
+++ src/app/views/home/live-list/live-list.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Live } from 'src/app/shared/model/live.model';
+import { LiveService } from 'src/app/shared/service/live.service';
+
+@Component({
+  selector: 'app-live-list',
+  templateUrl: './live-list.component.html',
+  styleUrls: ['./live-list.component.css']
+})
+export class LiveListComponent implements OnInit {
+
+    livesPrevious!: Live[];
+    livesNext!: Live[];
+
+  constructor(
+    public liveService: LiveService
+  ) { }
+
+  ngOnInit(): void {
+    this.getLives();
+  }
+
+  getLives(){
+    this.liveService.getLivesWithFlag('previous').subscribe(data =>{
+      this.livesPrevious = data.content;
+      console.log(this.livesPrevious);
+    });
+
+    this.liveService.getLivesWithFlag('next').subscribe(data =>{
+      this.livesNext = data.content;
+      console.log(this.livesNext);
+    });
+  }
+}
