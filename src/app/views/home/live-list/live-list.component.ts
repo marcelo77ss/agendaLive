bbea001aa02bb2893b30a99e1a6353690270a841
marcelo77ss <marcelo.77ss@gmail.com>
2021-09-13 23:19:53 -0300

    correçao, criando links

--- src/app/views/home/live-list/live-list.component.ts
+++ src/app/views/home/live-list/live-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { Live } from 'src/app/shared/model/live.model';
 import { LiveService } from 'src/app/shared/service/live.service';
 
@@ -13,7 +14,8 @@ export class LiveListComponent implements OnInit {
     livesNext!: Live[];
 
   constructor(
-    public liveService: LiveService
+    public liveService: LiveService,
+    public sanitizer: DomSanitizer
   ) { }
 
   ngOnInit(): void {
@@ -24,11 +26,17 @@ export class LiveListComponent implements OnInit {
     this.liveService.getLivesWithFlag('previous').subscribe(data =>{
       this.livesPrevious = data.content;
       console.log(this.livesPrevious);
+      this.livesPrevious.forEach(live => {
+        live.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(live.liveLink);
+      });
     });
 
     this.liveService.getLivesWithFlag('next').subscribe(data =>{
       this.livesNext = data.content;
       console.log(this.livesNext);
+      this.livesNext.forEach(live => {
+        live.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(live.liveLink);
+      });
     });
   }
 }
